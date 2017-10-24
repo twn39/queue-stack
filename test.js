3e40762c5ac9cbe29b618a64ac66d9f07c8ea5bb
Tang Weinan <twn39@163.com>
2017-10-24 16:26:38 +0800

    update

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 import test from 'ava'
-import { Queue } from './index'
+import { Queue, Stack } from './index'
 
 const queue = new Queue()
 
-test('push', t => {
+test('queue push', t => {
   t.is(queue.front(), null)
   t.is(queue.back(), null)
   queue.push('a')
@@ -13,20 +13,41 @@ test('push', t => {
   t.is(queue.front(), 'a')
 })
 
-test('pop', t => {
+test('queue pop', t => {
   queue.pop();
   t.is(queue.front(), 'b')
   t.is(queue.empty(), false)
 })
 
-test('size', t => {
+test('queue size', t => {
   queue.push('a')
   t.is(queue.size(), 4)
 })
 
-test('back', t => {
+test('queue back', t => {
   queue.push('b')
   t.is(queue.back(), 'b')
 })
 
 
+const stack = new Stack()
+
+test('stack push', t => {
+  t.is(stack.top(), null)
+  stack.push('a')
+  stack.push('b')
+  stack.push('c')
+  stack.push('d')
+  t.is(stack.top(), 'd')
+})
+
+test('stack pop', t => {
+  stack.pop();
+  t.is(stack.top(), 'c')
+  t.is(stack.empty(), false)
+})
+
+test('stack size', t => {
+  stack.push('a')
+  t.is(stack.size(), 4)
+})
