5f08daee6cda064f34fe8c2831b88e0e64a5b34f
Tang Weinan <twn39@163.com>
2017-10-24 16:16:19 +0800

    update

--- test.js
+++ test.js
@@ -1,23 +1,27 @@
 import test from 'ava'
-import Queue from './index'
+import { Queue } from './index'
 
-const queue = new Queue();
+const queue = new Queue()
 
 test('push', t => {
   t.is(queue.front(), null)
   t.is(queue.back(), null)
   queue.push('a')
+  queue.push('b')
+  queue.push('c')
+  queue.push('d')
   t.is(queue.front(), 'a')
 })
 
 test('pop', t => {
   queue.pop();
-  t.is(queue.empty(), true)
+  t.is(queue.front(), 'b')
+  t.is(queue.empty(), false)
 })
 
 test('size', t => {
   queue.push('a')
-  t.is(queue.size(), 1)
+  t.is(queue.size(), 4)
 })
 
 test('back', t => {
